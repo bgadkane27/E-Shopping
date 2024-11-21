2c7f021b94b4df7f3e68afb3bb8fb0e987772f29
Baburao Adkane <b.g.adkane27@gmail.com>
2024-11-21 18:59:19 +0530

    page not found added

--- client/src/pages/not-found/notfound.jsx
+++ client/src/pages/not-found/notfound.jsx
@@ -1,9 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { NavLink } from "react-router-dom";
+
 function NotFound() {
-    return(
-        <div>
-            Page not found
-        </div>
-    )
+  return (
+    <div className="flex items-center flex-col justify-center w-full h-screen">
+      <p className="text-9xl font-extrabold text-red-500">404</p>
+      <p className="text-4xl font-bold text-orange-600 py-2">
+        PAGE NOT FOUND.
+      </p>
+      <p className="text-md font-medium py-1">
+        The requested URL was not found on this server.
+      </p>
+      <Button className="mt-4">
+        <NavLink className="text-white hover:text-muted" to="/">
+          RETURN HOME
+        </NavLink>
+      </Button>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
