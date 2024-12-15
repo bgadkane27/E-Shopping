c2c434444befbecc1dcb698195156f598661cb8c
Baburao Adkane <b.g.adkane27@gmail.com>
2024-12-15 18:48:04 +0530

    Product

--- client/src/components/shop-view/header.jsx
+++ client/src/components/shop-view/header.jsx
@@ -2,8 +2,11 @@ import { Menu, Store } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
 
 function ShopHeader() {
+
+    const {isAuthenticated} = useSelector((state) => state.auth)
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex items-center justify-between h-16 px-4 md:px-6">
@@ -19,10 +22,17 @@ function ShopHeader() {
             </Button>
           </SheetTrigger>
           <SheetContent side="left" className="w-full max-w-xs">
-            <SheetTitle className="text-xl font-bold">Menu</SheetTitle>
+            <SheetTitle className="text-md font-semibold text-foreground">Menu</SheetTitle>
+
           </SheetContent>
         </Sheet>
       </div>
+      <div className="hidden lg:block">
+
+      </div>
+      {
+        isAuthenticated ? <div></div> : null
+      }
     </header>
   );
 }
