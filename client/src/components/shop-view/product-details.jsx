9d02a5e738dcf069fb2c7d1fe6a64c0b2028bedd
Baburao Adkane <b.g.adkane27@gmail.com>
2024-12-30 11:48:48 +0530

    Product popup

--- client/src/components/shop-view/product-details.jsx
+++ client/src/components/shop-view/product-details.jsx
@@ -1,13 +1,13 @@
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "../ui/button";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, StarIcon } from "lucide-react";
 import { Separator } from "../ui/separator";
-
+import { Avatar, AvatarFallback } from "../ui/avatar";
 
 function ProductDetailsDialog({ open, setOpen, productDetail }) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[60vw] ">
+      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[50vw] sm:max-w-[80vw] lg:max-w-[50vw]">
         <div className="relative overflow-hidden rounded-lg">
           <img
             src={productDetail?.image}
@@ -18,16 +18,52 @@ function ProductDetailsDialog({ open, setOpen, productDetail }) {
         <div>
           <div>
             <h2 className="text-3xl font-semibold">{productDetail?.name}</h2>
+            <p className="text-muted-foreground ml-1 mt-2 mb-2">
+              {productDetail?.description}
+            </p>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <span
+              className={`${
+                productDetail?.salesPrice > 0 ? "line-through" : ""
+              } text-lg font-semibold text-primary`}
+            >
+              ₹ {productDetail?.price}
+            </span>
+            {productDetail?.salesPrice > 0 ? (
+              <span className="text-lg font-semibold text-primary">
+                ₹ {productDetail?.salesPrice}
+              </span>
+            ) : null}
           </div>
           <div>
-            <p className="text-muted-foreground ml-1 mt-2 mb-2">{productDetail?.description}</p>
+            <Button variant="outline" size="sm" className="mt-2 w-full">
+              <ShoppingCart /> Add to Cart
+            </Button>
           </div>
-          <div>
-          <Button variant="outline" size="sm" className="mt-2">
-            <ShoppingCart /> Add to Cart
-          </Button>
+          <Separator className="my-4" />
+          <div className="max-h-[300px] overflow-auto">
+            <h3 className="text-md font-semibold my-2">Reviews</h3>
+            <div className="grid gap-6">
+              <div className="flex gap-4">
+                <Avatar>
+                  <AvatarFallback>AB</AvatarFallback>
+                </Avatar>
+                <div className="grid gap-1">
+                  <div className="flex gap-2">
+                    <h3 className="text-md font-semibold">Abhishek</h3>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <StarIcon className="w-4 h-4 fill-primary" />
+                    <StarIcon className="w-4 h-4 fill-primary" />
+                    <StarIcon className="w-4 h-4 fill-primary" />
+                    <StarIcon className="w-4 h-4 fill-primary" />
+                    <StarIcon className="w-4 h-4 fill-primary" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <Separator className="my-4"/>
         </div>
       </DialogContent>
     </Dialog>
